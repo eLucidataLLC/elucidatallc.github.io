4d7570cab1c9eeac74473c976b8f033b17f42e67
elpbatista <elpbatista@gmail.com>
2021-01-31 20:03:13 -0800

    updating website

--- README.md
+++
@@ -1,1 +0,0 @@
-# eLucidata LLC
---
+++ main.js
@@ -0,0 +1,9 @@
+var slider = tns({
+   items: 3,
+   slideBy: 1,
+   autoplay: true,
+   controls: false,
+   autoplayButtonOutput: false,
+   nav: false,
+   speed: 1500
+ });
