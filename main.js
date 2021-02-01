7ac76c9ed9bb1e1d0368e3b5e6ada25b53c07e78
elpbatista <elpbatista@gmail.com>
2021-02-01 14:20:38 -0800

    making slider responsive

--- main.js
+++ main.js
@@ -5,5 +5,17 @@ var slider = tns({
    controls: false,
    autoplayButtonOutput: false,
    nav: false,
-   speed: 1500
+   speed: 1500,
+   responsive: {
+     360: {
+       items: 1,
+       gutter: 0
+     },
+     640: {
+       items: 2
+     },
+     770: {
+       items: 3
+     }
+   }
  });
